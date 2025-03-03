46f483423a857c9073d6002326df8cfcc25253ef
Andrew Stamm <andrewstamm@me.com>
2025-03-03 09:38:13 -0600

    Initial Commit

---
+++ reviewable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  reviewable
+//
+//  Created by Andrew Stamm on 3/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ reviewable/reviewableApp.swift
@@ -0,0 +1,17 @@
+//
+//  reviewableApp.swift
+//  reviewable
+//
+//  Created by Andrew Stamm on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct reviewableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
